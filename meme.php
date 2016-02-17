5fa60f67f8826ea01573035d5ee21b62c631db53
Nix <domdgibs@ut.utm.edu>
2016-02-17 00:20:45 +0000

    memegen.php made everything work except json and javascript

--- meme.php
+++ meme.php
@@ -1,190 +1,69 @@
 
 <!DOCTYPE html>
 <html>
-    
-    
-    
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>MEME</title>
-        <meta name="description" content="An interactive getting started guide for Brackets.">
-        <link rel="stylesheet" href="main.css">
-        
-        
-    
+     <head>
+        <link rel="stylesheet" type="text/css" href="meme.css">
     </head>
-
     
-    <?php
-class MemeGenerator{
-	
-	private $upperText;
-	private $lowerText;
-	private $alignment;
-	private $background;
-	private $font = 'impact.ttf';
-	private $im;
-	private $imgSize;
-	public function setUpperText($txt)		 { $this->upperText = strtoupper($txt); }
-	public function setLowerText($txt) 		 { $this->lowerText = strtoupper($txt); }
-	
-	private function getHorizontalTextAlignment($imgWidth,$topRightPixelOfText)
-	{
-		return ceil(($imgWidth - $topRightPixelOfText) / 2);
-	}
-	
-	private function CheckTextWidthExceedImage($imgWidth,$fontWidth) {
-	
-			if($imgWidth < $fontWidth + 20 )
-				 return true;
-			else 
-				 return false;
-	}
-	
-	private function GetFontPlacementCoordinates($text,$fontSize)
-	{
-		return imagettfbbox($fontSize, 0, $this->font , $text);
-	}
-	private function ReturnImageFromPath($path)
-	{
-		$ext = pathinfo($path, PATHINFO_EXTENSION);
-		if($ext == 'jpg' || $ext == 'jpeg')
-			return imagecreatefromjpeg($path);
-		else if($ext == 'png')
-			return imagecreatefrompng($path);
-		else if($ext == 'gif')
-			return imagecreatefromgif($path);
-	}
-	public function __construct($path)
-	{
-		$this->im = $this->ReturnImageFromPath($path);
-		$this->imgSize = getimagesize($path);
-		
-		$this->background = imagecolorallocate($this->im, 255, 255, 255);
-		imagecolortransparent($this->im, $this->background);
-	}
-	
-	private function WorkOnImage($text,$size,$type)
-	{
-			if($type == "upper")
-				$TextHeight = 35;
-			else
-				$TextHeight = $this->imgSize[1] - 20;
-		while(1)
-		{
-			//get coordinate for the text
-			$coords = $this->GetFontPlacementCoordinates($text,$size);
-			// place the text in center
-			if($type == "upper")
-				$UpperTextX = $this->getHorizontalTextAlignment($this->imgSize[0],$coords[4]);
-			else
-				$LowerTextX = $this->getHorizontalTextAlignment($this->imgSize[0],$coords[4]); 
-			//check if the text does not exceed image width if yes then repeat with size = size - 1
-			if($this->CheckTextWidthExceedImage($this->imgSize[0],$coords[2] - $coords[0]))
-			{
-				if($type == "upper")
-					$TextHeight = $TextHeight - 1; 		//if it is top text take it up as font size decreases
-				else
-					$TextHeight = $TextHeight + 1; 		//if it is bottom text take it down as font size decreases		
-				if($size == 10)		
-				{
-					//if text size is reached to lower limit and still it is exceeding image width start breaking into lines
-					if($type == "upper")
-					{
-						$this->upperText= $this->ReturnMultipleLinesText($text,$type,16);
-						$text = $this->upperText;
-						return;
-					}
-					else
-					{
-						$this->lowerText= $this->ReturnMultipleLinesText($text,$type,$this->imgSize[1] - 20);
-						$text = $this->lowerText;
-						return;
-					}
-				}
-				else
-					$size -=1;
-			}
-			else
-				break;
-		}
-		if($type == "upper")
-			$this->PlaceTextOnImage($this->im,$size, $UpperTextX, $TextHeight,$this->font, $this->upperText);
-		else
-			$this->PlaceTextOnImage($this->im,$size, $LowerTextX, $TextHeight,$this->font, $this->lowerText);
-	}
-	
-	private function PlaceTextOnImage($img,$fontsize,$Xlocation,$Textheight,$font,$text)
-	{
-		imagettftext($this->im,$fontsize,0,$Xlocation,$Textheight, (int)$this->background, $font, $text);		
-	}
-	
-	private function ReturnMultipleLinesText($text,$type,$textHeight)
-	{
-		//breaks the whole sentence into multiple lines according to the width of the image.
-			
-			
-			//break sentence into an array of words by using the spaces as params
-			$brokenText = explode(" ",$text);
-			$finalOutput = "";
-			
-				if($type != "upper")
-					$textHeight =$this->imgSize[1] - ((count($brokenText)/2) * 3);
-			
-			for($i = 0 ; $i < count($brokenText) ; $i++)
-			{	
-				$temp 		 = $finalOutput;
-				$finalOutput.= $brokenText[$i]." ";
-				// this will help us to keep the last word in hand if this word is the cause of text exceeding the image size.			
-				// We will be using this to append in next line.
-				
-				//check if word is too long i.e wider than image width
-				
-				//get the sentence(appended till now) placement coordinates
-				$dimensions = $this->GetFontPlacementCoordinates($finalOutput,10);
-				//check if the sentence (till now) is exceeding the image with new word appended
-				if($this->CheckTextWidthExceedImage($this->imgSize[0],$dimensions[2] - $dimensions[0]))	//yes it is then
-				{
-					// append the previous sentence not with the new word  ( new word == $brokenText[$i] )
-					$dimensions = $this->GetFontPlacementCoordinates($temp,10);
-					$locx = $this->getHorizontalTextAlignment($this->imgSize[0],$dimensions[4]);
-					$this->PlaceTextOnImage($this->im,10,$locx,$textHeight,$this->font,$temp);
-					$finalOutput = $brokenText[$i];
-					$textHeight +=13;
-				}
-				
-				//if this is the last word append this also.The previous if will be true if the last word will have no room
-				if($i == count($brokenText) - 1)
-				{
-					$dimensions = $this->GetFontPlacementCoordinates($finalOutput,10);
-					$locx = $this->getHorizontalTextAlignment($this->imgSize[0],$dimensions[4]);
-					$this->PlaceTextOnImage($this->im,10,$locx,$textHeight,$this->font,$finalOutput);
-				}
-			}
-			return $finalOutput;		
-	}
-	
-	public function processImg()
-	{
-		if($this->lowerText != "")
-		{
-			$this->WorkOnImage($this->lowerText,30,"lower");
-		}
-		if($this->upperText != "")
-		{
-			$this->WorkOnImage($this->upperText,30,"upper");
-		}
-		imagejpeg($this->im,"abc.jpg");
-		imagedestroy($this->im);
-		
-		echo "abc.jpg";
-	}
-}
-$obj = new MemeGenerator('Blue.jpg');
-$upmsg   = $_GET['upmsg'];
-$downmsg = $_GET['downmsg'];
-$obj->setUpperText($upmsg);
-$obj->setLowerText($downmsg);
-$obj->processImg();
-?>
+    <body>
+        
+
+         <img src="Blue.jpg">
+      <br>
+        <select name = "img">
+        <option value="blue.jpg"> blue</option>
+        <option value = "huey.jpg"> huey</option>
+        </select>
+        
+
+        <form action = memegen.php method = "get">
+            
+                <div id="selection" style="display:inline-block; width:100px">
+        
+        <br>
+              <div style="display:inline-block; width:100px">
+            
+            <input type = "text" name = "top"> 
+           <input type = "text" name = "bottom">
+            <br>
+            
+            <input type=submit>
+        
+                  
+                  <input type="color" name="color" value="#FFFFFF"></input>
+                    
+                    
+                    <!-----top and bottom text------>
+        <p id = "upper" style = "position:absolute; top: 2px; left: 50px; text-align: left; word-wrap:break-word;">
+            <p id = "upper" style = "position: absolute; top:2px; left: 50px; font-size:50px; word-wrap:break-word; color: <?php echo $_GET["color"]?>; text-align: left">
+                <?php echo $_GET["top"];?>
+            </p>
+                  
+                    
+                    
+             <br>
+            
+            <p id = "lower" style = "position:absolute; top: 350px; left: 65px; font-size:50px; word-wrap:break-word;
+ text-align:left">
+            <p id = "lower" style = "position: absolute; font-size:50px; word-wrap:break-word;
+top:350px; left:65; color: <?php echo $_GET["color"]?>; text-align:left">
+                <?php echo $_GET["bottom"];?>
+            
+                
+            </p>
+          <!----- end top and bottom text------>
+            <ul>
+            
+            
+            <li><a href="?http://cs1.utm.edu/~domdgibs/memegen.php?img=blue.jpg">Blue Ivy</a></li>
+                
+         <li><a href="?http://cs1.utm.edu/~domdgibs/memegen.php?img=huey">Huey N.</a></li>
+            
+        </ul>
+        
+    </body>
+
+</html>
+    
+    
+    
